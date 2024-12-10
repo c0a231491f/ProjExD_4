3f2dbabbec61d03fbda07f95c000cd086398cfa8
C0B23003 <c0b23003ec@edu.teu.ac.jp>
2024-12-10 17:42:54 +0900

    追加機能4merge

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -72,14 +72,8 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = xy
         self.speed = 10
-<<<<<<< HEAD
         self.state = "normal"
         self.hyper_life = 0
-=======
-        
-
-        
->>>>>>> C0B23003/feature1
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -101,7 +95,8 @@ class Bird(pg.sprite.Sprite):
             self.speed = 20
         else:
             self.speed = 10
-        
+
+
         for k, mv in __class__.delta.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
@@ -113,7 +108,6 @@ class Bird(pg.sprite.Sprite):
             self.dire = tuple(sum_mv)
             self.image = self.imgs[self.dire]
         screen.blit(self.image, self.rect)
-        #ss
 
 
 class Bomb(pg.sprite.Sprite):
@@ -246,7 +240,7 @@ class Score:
     def __init__(self):
         self.font = pg.font.Font(None, 50)
         self.color = (0, 0, 255)
-        self.value = 0
+        self.value = 1000
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         self.rect = self.image.get_rect()
         self.rect.center = 100, HEIGHT-50
